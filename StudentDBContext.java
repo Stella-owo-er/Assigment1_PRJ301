87f3c163f82d17077a01c0a5a152d15b9bb41129
Stella-owo-er <101204024+Stella-owo-er@users.noreply.github.com>
2023-03-22 14:21:46 +0700

    Add files via upload

---
+++ StudentDBContext.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Student;
+
+/**
+ *
+ * @author DAT
+ */
+public class StudentDBContext extends DBContext<Student> {
+
+    public ArrayList<Student> listStudentInAGroup(int id) {
+        ArrayList<Student> liststudent = new ArrayList<>();
+        try {
+            String sql = "SELECT s.stdid,s.stdcode,s.stdfirstname,s.stdmidname,s.stdlastname,s.stdgmail\n"
+                    + "FROM Student s\n"
+                    + "INNER JOIN Student_Group stdg ON stdg.stdid = s.stdid\n"
+                    + "INNER JOIN [Group] g ON g.gid = stdg.gid\n"
+                    + "WHERE g.gid = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, id);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+
+                Student s = new Student();
+                s.setStdid(rs.getString("stdid"));
+               s.setStdname(rs.getString("stdname"));
+                s.setStdgmail(rs.getString("stdgmail"));
+
+                liststudent.add(s);
+
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(SessionDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return liststudent;
+    }
+
+    @Override
+    public void insert(Student model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(Student model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(Student model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    
+
+    @Override
+    public ArrayList<Student> list() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Student get(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
